565e552c87a6f7ef1e983dc4648fcabece271934
Brad <bradcochi@gmail.com>
2023-11-23 13:27:37 -0500

    icons added to Topbar component

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -21,9 +21,32 @@ const Topbar = () => {
         dispaly="flex"
         backgrooundColor={colors.primary[400]}
         borderRadius="3px"
-      ></Box>
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
 
-      <IconButton></IconButton>
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
