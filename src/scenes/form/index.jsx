6e1dfe642df3711b1bbcfffc8467a2b74347117d
Brad <bradcochi@gmail.com>
2023-11-25 17:20:33 -0500

    added extra validation for email and contact on Form page

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -13,11 +13,17 @@ const initialValues = {
   address2: "",
 };
 
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
 const userSchema = yup.object().shape({
   firstName: yup.string().required("required"),
   lastName: yup.string().required("required"),
-  email: yup.string().required("required"),
-  contact: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
   address1: yup.string().required("required"),
   address2: yup.string().required("required"),
 });
