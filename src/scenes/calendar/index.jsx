0f932cf0aed3d024c47887d36d0c91936a6379e5
Brad <bradcochi@gmail.com>
2023-11-25 18:25:24 -0500

    created directory and file for Calendar page

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import FullCallendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/header";
+import { tokens } from "../../theme";
+
+const Calendar = () => {};
+
+export default Calendar;
