74a23402c6818c8d203bc83601e5def5964a5676
Brad <bradcochi@gmail.com>
2023-11-26 15:43:41 -0700

    added if statement to Calendar function to handle adding event

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -24,6 +24,16 @@ const Calendar = () => {
     const title = prompt("Please enter a new title for your event");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
   };
 };
 
