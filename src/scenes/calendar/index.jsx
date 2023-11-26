a42aac820347cee995e06b349bfdc35ffa8f5b45
Brad <bradcochi@gmail.com>
2023-11-26 15:55:19 -0700

    added Header to Calendar page

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -45,6 +45,12 @@ const Calendar = () => {
       selected.event.remove();
     }
   };
+
+  return (
+    <Box m="20px">
+      <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+    </Box>
+  );
 };
 
 export default Calendar;
