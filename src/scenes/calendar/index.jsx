ee0f14b3a9a6a5321041505202a3d04fdebc3f88
Brad <bradcochi@gmail.com>
2023-11-26 16:11:45 -0700

    changed ListItem tag to be self-closing, remember this for future projects

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -82,7 +82,7 @@ const Calendar = () => {
                       }
                     </Typography>
                   }
-                ></ListItemText>
+                />
               </ListItem>
             ))}
           </List>
