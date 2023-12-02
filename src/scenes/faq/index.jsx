558a33d4bb287899265efd10d3e87ed24cbb1159
Brad <bradcochi@gmail.com>
2023-12-01 18:12:55 -0700

    established import statements and function for FAQ page

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,11 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/header";
+import Accordion from "@mui/material/Accordiaon";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+const FAQ = () => {};
+
+export default FAQ;
