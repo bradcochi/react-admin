8fee5af6a7231bd3e1dc3b4927b73c5a19ea8967
Brad <bradcochi@gmail.com>
2023-12-02 18:08:45 -0700

    added header to FAQ page

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -9,6 +9,12 @@ import { tokens } from "../../theme";
 const FAQ = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions" />
+    </Box>
+  );
 };
 
 export default FAQ;
