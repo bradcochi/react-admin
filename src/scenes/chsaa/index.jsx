74321fb137c29fc7e78d117afd86a350b8e21635
Brad <bradcochi@gmail.com>
2023-12-06 11:43:59 -0700

    fixed issues with fless property

--- src/scenes/chsaa/index.jsx
+++ src/scenes/chsaa/index.jsx
@@ -1,14 +1,43 @@
-import { Box } from "@mui/material";
+import * as React from "react";
+import { Box, Typography } from "@mui/material";
 import Header from "../../components/header";
+import Button from "@mui/material/Button";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
 const Chsaa = () => {
   return (
     <Box m="20px">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Box>
         <Header
           title="CHSAA UI"
           subtitle="Testing out components for CHSAA Seeding UI"
         />
+      </Box>
+      <Box display="flex">
+        <FormGroup>
+          <Typography>Baseball</Typography>
+
+          <FormControlLabel
+            control={<Switch defaultChecked />}
+            label="Showing"
+          />
+          <Button variant="contained" color="success">
+            Send to Database
+          </Button>
+        </FormGroup>
+        <FormGroup>
+          <Typography>Baseball</Typography>
+
+          <FormControlLabel
+            control={<Switch defaultChecked />}
+            label="Showing"
+          />
+          <Button variant="contained" color="success">
+            Send to Database
+          </Button>
+        </FormGroup>
       </Box>
     </Box>
   );
